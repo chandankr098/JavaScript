c94651a3d9010a608e87cff22f6a48c1b936ba94
Chandan verma <165891259+chandankr098@users.noreply.github.com>
2025-01-29 12:32:13 +0000

    container confif done

---
+++ Basic/conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "54abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+//let score = 0
+//let score = undefined
+//let score = true
+//let score ="chandan"
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "45" = 45
+// "43abg" = NaN
+// true = 1; false = 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false
+// "" = false
+// "name" = true
+
+
+let someNumber = 23
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
