4563fbe6c29c000bf766109be342d6d435ed5e7a
Chandan verma <165891259+chandankr098@users.noreply.github.com>
2025-01-29 11:08:04 +0000

    container config done

---
+++ Basic/variables.js
@@ -0,0 +1,14 @@
+const accountId =  123453
+let accountEmail = "chandan@gmail.com"
+var accoutnPassword = "09876"
+accountCity = "Delhi"
+
+// accountId = 2 // not allowed
+
+accountEmail = "CK@gmail.com"
+accountPassword = "12345"
+accountCity = "Patna"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])  
